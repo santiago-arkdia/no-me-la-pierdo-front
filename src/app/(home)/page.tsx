995e948e94f239754cbbda9314098c32a562e43e
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-06 00:51:52 -0500

    Limpieza-del-proyecto---inicio-definicion-de-Colores

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        <h1>Estamos vivos</h1>
+        <h2>desde (home)</h2>
+      </div>
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
+      colors: {
+        background: "var(--background)",
+        white: "var(--white)",
+        blue: "var(--blue)",
+        gray: "var(--gray)",
+      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [],
