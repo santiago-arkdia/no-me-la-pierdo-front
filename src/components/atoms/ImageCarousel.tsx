63abed9427a6338cc1497d26a4ac448da405ff93
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-08 22:58:40 -0500

    Feature: Carousel, efectos y decoracion.

---
+++ src/components/atoms/ImageCarousel.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { IoMdCalendar } from "react-icons/io";
+
+export const ImageCarousel = ({ data }: any) => {
+  return (
+    <div className="relative w-full h-full">
+      <Image
+        className="absolute inset-0 z-0"
+        src={data.image}
+        alt="@"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-background via-transparent to-background opacity-100"></div>
+      <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-t from-background via-transparent to-background opacity-50"></div>
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-end px-6 pb-6 text-center ">
+        <h2 className="text-5xl mb-4 w-80  tracking-widest">{data.name}</h2>
+        <div className="flex  mb-4  justify-between w-80 font-light">
+          <p className="flex items-center">
+            <IoMdCalendar className="mr-2" />
+            {data.date}
+          </p>
+          <p className="flex items-center">
+            <FaMapMarkerAlt className="mr-2" />
+            {data.place}
+          </p>
+        </div>
+        <button
+          type="button"
+          className="  border-blue border-2  hover:text-blue focus:ring-1 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-base px-6 py-1 text-center me-2 mb-2 "
+        >
+          Comprar Entrada
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/molecules/HomeCarousel.tsx
+++ src/components/molecules/HomeCarousel.tsx
@@ -1,53 +1,107 @@
 // import Swiper core and required modules
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import { EffectFade } from "swiper/modules";
+// import Image from "next/image";
+// const HomeCarousel = () => {
+//   return (
+//     <div className="mx-auto   w-3/4 h-[756px] bg-blue-600">
+//       <Swiper
+//         effect="fade"
+//         spaceBetween={50}
+//         slidesPerView={1}
+//         navigation
+//         autoplay={{ delay: 3000 }}
+//         allowTouchMove={false}
+//         loop={true}
+//         // scrollbar={}
+//         // scrollbar={{ draggable: true }}
+//         onSwiper={(swiper) => console.log(swiper)}
+//         onSlideChange={() => console.log("slide change")}
+//         className="h-full w-full sm:w-full"
+//         modules={[Autoplay, Pagination, Navigation, EffectFade]}
+//       >
+//         <SwiperSlide>
+//           <div className="h-full">
+//             <Image src="/images/jirafa.jpg" alt="@" fill objectFit="cover" />
+//           </div>
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <div className="h-full">
+//             <Image src="/images/suricata.jpg" alt="@" fill objectFit="cover" />
+//           </div>
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <div className="h-full">
+//             <Image src="/images/elephant.jpg" alt="@" fill objectFit="cover" />
+//           </div>
+//         </SwiperSlide>
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default HomeCarousel;
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
 import Image from "next/image";
+import { ImageCarousel } from "../atoms/ImageCarousel";
+
+const dataCarousel = {
+  carousel: {
+    list: [
+      {
+        name: "Bioparke Ukumari",
+        date: "May 11-12, 2024",
+        place: "Pereira, Colombia",
+        url: "#",
+        image: "/images/jirafa.jpg",
+      },
+      {
+        name: "Bioparke Ukumari",
+        date: "May 12-13, 2024",
+        place: "Pereira, Colombia",
+        url: "#",
+        image: "/images/suricata.jpg",
+      },
+      {
+        name: "Bioparke Ukumari",
+        date: "May 13-14, 2024",
+        place: "Pereira, Colombia",
+        url: "#",
+        image: "/images/elephant.jpg",
+      },
+    ],
+  },
+};
+
 const HomeCarousel = () => {
   return (
-    <div className="mx-auto   w-3/4 h-[756px] bg-blue-600">
+    <div className="mx-auto w-3/4 h-[756px] bg-blue-600">
       <Swiper
+        effect="fade"
         spaceBetween={50}
         slidesPerView={1}
-        navigation
-        autoplay={{ delay: 5000 }}
         allowTouchMove={false}
+        autoplay={{ delay: 5000 }}
         loop={true}
-        // scrollbar={}
-        // scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-        className="h-full w-full sm:w-full bg-red-500"
-        modules={[Autoplay, Pagination, Navigation]}
+        className="h-full w-full sm:w-full"
+        modules={[Autoplay, Pagination, Navigation, EffectFade]}
       >
-        <SwiperSlide>
-          <div className="h-full bg-blue-50">
-            <Image
-              src="/images/jirafa.jpg"
-              alt="@"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>
-          <div className="h-full bg-blue-50">
-            <Image
-              className="-z-10"
-              src="/images/jirafa.jpg"
-              alt="@"
-              layout="fill"
-              objectFit="cover"
-            />
-            <h1>holiii</h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {dataCarousel.carousel.list.map((data, index) => (
+          <SwiperSlide key={index}>
+            <ImageCarousel data={data} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
