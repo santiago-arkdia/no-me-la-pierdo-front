7cc519afdd4b7a2efe9793638ca4456d52ed5590
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-09 00:12:30 -0500

    Featurue: creacion archivos. prueba para calendario

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,16 @@
+import { Calendar } from "app/components/molecules/Calendar";
+import { CardSell } from "app/components/molecules/CardSell";
+
 export default function Home() {
   return (
     <main className="">
       <div>
         <h1>Estamos vivos</h1>
         <h2>desde (home)</h2>
+        <section>
+          <CardSell />
+          <Calendar />
+        </section>
       </div>
     </main>
   );
---
+++ src/components/molecules/Calendar.tsx
@@ -0,0 +1,3 @@
+export const Calendar = () => {
+  return <h1>Calendario</h1>;
+};
