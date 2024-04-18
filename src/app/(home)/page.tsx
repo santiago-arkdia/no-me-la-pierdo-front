45ea18378063b05ce873d6feec77512cc8c909be
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-18 18:37:01 -0500

    Fix: Ajuste la Home para que mejore la experienciaen pantallas mas pequenas.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     <main>
       <div>
         <HomeCarousel />
-        <section className="flex flex-col md:flex-row justify-center items-start px-4 md:px-48">
+        {/* <section className="flex flex-col md:flex-row justify-center items-start px-4 md:px-48">
           <div className="w-full md:w-1/3 ">
             <Calendar
               onDateSelected={handleDateSelected}
@@ -26,6 +26,17 @@ export default function Home() {
             />
           </div>
           <div className="w-full md:w-2/3">
+            <CardSellContainer selectedDate={selectedDate} />
+          </div>
+        </section> */}
+        <section className="flex flex-col md:flex-row justify-center items-start gap-2 px-4 sm:px-6 md:px-12 lg:px-0">
+          <div className="w-full lg:w-1/3  lg:mb-0 lg:pr-2">
+            <Calendar
+              onDateSelected={handleDateSelected}
+              selectedDate={selectedDate}
+            />
+          </div>
+          <div className="w-full lg:w-2/3 lg:pl-2">
             <CardSellContainer selectedDate={selectedDate} />
           </div>
         </section>
