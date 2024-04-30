5df5c3e68c5cb4ad875141827ca0a9c8f7484c69
Santiago Millan <santiagomillanpardo@gmail.com>
2024-04-29 22:15:14 -0500

    Feature:  ajuste eliminar elementos del carro de compras

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -16,6 +16,32 @@ type Store = {
 
 export const useShoppingCart = create<Store>((set) => ({
   cart: [],
+  // addToCart: (cartItem: CartItem) =>
+  //   set((state) => {
+  //     const existingItemIndex = state.cart.findIndex(
+  //       (item) => item.id === cartItem.id
+  //     );
+  //     let newCart;
+  //     if (existingItemIndex >= 0) {
+  //       if (cartItem.quantity <= 0) {
+  //         newCart = [
+  //           ...state.cart.slice(0, existingItemIndex),
+  //           ...state.cart.slice(existingItemIndex + 1),
+  //         ];
+  //       } else {
+  //         newCart = [
+  //           ...state.cart.slice(0, existingItemIndex),
+  //           { ...state.cart[existingItemIndex], quantity: cartItem.quantity },
+  //           ...state.cart.slice(existingItemIndex + 1),
+  //         ];
+  //       }
+  //     } else if (cartItem.quantity > 0) {
+  //       newCart = [...state.cart, cartItem];
+  //     } else {
+  //       return {};
+  //     }
+  //     return { cart: newCart };
+  //   }),
   addToCart: (cartItem: CartItem) =>
     set((state) => {
       const existingItemIndex = state.cart.findIndex(
@@ -38,7 +64,7 @@ export const useShoppingCart = create<Store>((set) => ({
       } else if (cartItem.quantity > 0) {
         newCart = [...state.cart, cartItem];
       } else {
-        return {};
+        return state;
       }
       return { cart: newCart };
     }),
